d7547caf18a1cfd3b7557090687f69e06d9617c4
Brian Cañón Rojas <41877674+xCopyDogsx@users.noreply.github.com>
2021-01-26 17:12:51 -0500

    Helpers y base URL

--- Config/Config.php
+++ Config/Config.php
@@ -6,5 +6,10 @@
  	const DB_USER = "root";
  	const DB_PASSWORD = "";
  	const DB_CHARSET = "chartset=utf8";
- 	
+ 	//Delimitadores decimal y millar 24.123,50
+ 	const SPD = ",";
+ 	const SPM = ".";
+ 	//Simbolo de la moneda 
+ 	const SMONEY = "$COP";
+
  ?>
