71b9d560675bf46a8d70a70e20612d2514ec6ae9
Brian Cañón Rojas <41877674+xCopyDogsx@users.noreply.github.com>
2021-01-26 16:16:49 -0500

    Consts

--- Config/Config.php
+++ Config/Config.php
@@ -1,6 +1,10 @@
 <?php 
 	//define("BASE_URL","http://localhost/ProntoMueble/");
 	const BASE_URL = "http://localhost/ProntoMueble";
- 
+ 	const DB_HOST = "localhost";
+ 	const DB_NAME = "prontomueble";
+ 	const DB_USER = "root";
+ 	const DB_PASSWORD = "";
+ 	const DB_CHARSET = "chartset=utf8";
  	
  ?>
