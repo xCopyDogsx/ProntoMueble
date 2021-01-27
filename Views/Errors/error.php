099b6488e467adfe7fbee8ce1bed31c3c7b5be05
Brian Cañón Rojas <41877674+xCopyDogsx@users.noreply.github.com>
2021-01-27 14:14:00 -0500

    Se añade la página de error 403

--- Views/Errors/error.php
+++ Views/Errors/error.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-	<title></title>
+	<title>Error: 404</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"> 
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
