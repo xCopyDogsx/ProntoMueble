21f5dc0f7bc94f7eb00f8a3a7b3cf3c7449bc42b
Brian Cañón Rojas <41877674+xCopyDogsx@users.noreply.github.com>
2021-01-27 16:04:02 -0500

    Cambios menores

--- Views/Template/header_admin.php
+++ Views/Template/header_admin.php
@@ -9,4 +9,4 @@
     <link rel="stylesheet" type="text/css" href="<?php echo media(); ?>/css/main.css">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  </head>
+  </head>             
