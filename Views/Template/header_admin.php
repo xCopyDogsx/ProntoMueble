3aaf75c250a017e53030a3426f994d24f2e1e57f
Brian Cañón Rojas <41877674+xCopyDogsx@users.noreply.github.com>
2021-02-02 14:03:48 -0500

    Se modifica la plantilla S7-33

--- Views/Template/header_admin.php
+++ Views/Template/header_admin.php
@@ -9,4 +9,22 @@
     <link rel="stylesheet" type="text/css" href="<?php echo media(); ?>/css/main.css">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  </head>             
+    <link rel="shortcut icon" href="<?php echo media(); ?>/images/icono.png">
+  </head>
+    <body class="app sidebar-mini">
+    <!-- Navbar-->
+    <header class="app-header"><a class="app-header__logo" href="<?= base_url(); ?>/dashboard">ProntoMueble</a>
+      <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+      <!-- Navbar Right Menu-->
+      <ul class="app-nav">
+       
+        <!-- User Menu-->
+        <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
+          <ul class="dropdown-menu settings-menu dropdown-menu-right">
+            <li><a class="dropdown-item" href="<?= base_url(); ?>/opciones"><i class="fa fa-cog fa-lg"></i> Configuración</a></li>
+            <li><a class="dropdown-item" href="<?= base_url(); ?>/perfil"><i class="fa fa-user fa-lg"></i> Perfil</a></li>
+            <li><a class="dropdown-item" href="<?= base_url(); ?>/logout"><i class="fa fa-sign-out fa-lg"></i> Salir</a></li>
+          </ul>
+        </li>
+      </ul>
+    </header>             
