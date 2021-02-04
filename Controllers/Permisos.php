a8f2389757a06adf1b4e2bc2553d11c15867542a
Brian Cañón Rojas <41877674+xCopyDogsx@users.noreply.github.com>
2021-02-04 17:57:32 -0500

    Update Permisos.php

--- Controllers/Permisos.php
+++ Controllers/Permisos.php
@@ -29,7 +29,7 @@
 					}
 				$arrPermisoRol['modulos']=$arrModulos;
 					
-				$html=getModal('modalPermisos',$arrPermisoRol);
+				$html=getModal("modalPermiso",$arrPermisoRol);
 			}
 			die();
 		}
