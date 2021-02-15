4941cdcf938d97a53b6ed260de4eb72ce4f76c77
Brian Cañón Rojas <41877674+xCopyDogsx@users.noreply.github.com>
2021-02-15 15:44:40 -0500

    Update home.php

--- Views/home.php
+++ Views/home.php
@@ -132,8 +132,7 @@
 			</div>
 			<!-- Load more -->
 			<div class="flex-c-m flex-w w-full p-t-45">
-				<a href="#" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
-					Load More
+				<a href="<?= base_url();?>/tienda" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">Muestrame más
 				</a>
 			</div>
 		</div>
